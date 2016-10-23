d198719af04b7c6f23f46862360c485648aa361e
akameco <akameco.t@gmail.com>
2016-10-23 20:20:09 +0900

    Add 最高かよ

--- scripts/misc.js
+++ scripts/misc.js
@@ -4,4 +4,8 @@ module.exports = robot => {
 	robot.hear(/^tgif$/i, msg => {
 		msg.send('https://files.slack.com/files-pri/T02TM1NQZ-F2SAJQFE1/slack_for_ios_upload.jpg');
 	});
+
+	robot.hear(/^最高かよ$/i, msg => {
+		msg.send('https://files.slack.com/files-pri/T02TM1NQZ-F2SB6JLN5/2958.jpg');
+	});
 };
