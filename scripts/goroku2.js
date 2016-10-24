092a91b9fbaed6f9815423ba4e57adaa176b49b5
elzup <guild0105@gmail.com>
2016-10-24 23:15:36 +0900

    Create goroku2

---
+++ scripts/goroku2.js
@@ -0,0 +1,54 @@
+// Description:
+//   ユーザ語録
+// Commands:
+//   goroku2 <name> add <message> - 語録を追加
+//   goroku2 <name> del <message> - 語録を削除
+//   goroku2 <name> random <message> - 語録をランダム表示
+//   goroku2 <name> list <message> - 語録一覧
+
+'use strict';
+
+module.exports = robot => {
+	robot.brain.autoSave = true;
+
+	const loadLogger = (name) => {
+		addName(name);
+		return robot.brain.get(`goroku_${name}`) || [];
+	};
+
+	const addName = (name) => {
+		logger = robot.brain.get('gorokuNames') || [];
+		if (logger.contains(name)) { return }
+		logger.push(name);
+		robot.brain.set('gorokuNames', logger);
+	};
+
+	robot.hear(/^goroku2 add (.+) (.+)/i, msg => {
+		const [name, m] = msg.match;
+		const logger = loadLogger(name);
+		logger.push(m);
+		robot.brain.set(logger_name, logger);
+		msg.send(`${name} ${m}`);
+	});
+
+	robot.hear(/^goroku2 del (.+) (.+)$/, msg => {
+		const [name, m] = msg.match;
+		const logger = loadLogger(name);
+		const nextLogger = logger.filter(mes => mes !== m);
+		robot.brain.set('goroku', nextLogger);
+	});
+
+	robot.hear(/^goroku2 random (.+)$/i, msg => {
+		const name = msg.match;
+		const logger = loadLogger(name);
+		const mes = logger[Math.floor(Math.random() * logger.length)];
+		msg.send(`:iwi: ${mes}`);
+		robot.send(`${name} ${m}`);
+	});
+
+	robot.hear(/^goroku2 list (.+)$/i, msg => {
+		const name = msg.match;
+		const logger = loadLogger(name);
+		msg.send(`${logger.map(v => `${name} ${v}`).join('\n')}`);
+	});
+};
