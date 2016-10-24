7444efeb36455ebfedda1145e1ec8ae1a48ac31d
akameco <akameco.t@gmail.com>
2016-10-25 03:05:41 +0900

    fix 完全一致

--- scripts/mahouka.js
+++ scripts/mahouka.js
@@ -9,7 +9,7 @@ const cheerio = require('cheerio');
 const BASE = 'http://mahouka.jp/special/s07.html';
 
 module.exports = robot => {
-	robot.hear(/魔法科|お兄様/i, msg => {
+	robot.hear(/^(魔法科|お兄様)$/i, msg => {
 		got(BASE).then(res => {
 			const $ = cheerio.load(res.body);
 			const imgs = $('li > img').map((v, el) => {
