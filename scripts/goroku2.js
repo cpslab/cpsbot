43bdece0c900bfa7091661f3e1fe271765a0521f
elzup <guild0105@gmail.com>
2016-10-25 01:29:41 +0900

    Fix goroku2

--- scripts/goroku2.js
+++ scripts/goroku2.js
@@ -1,10 +1,10 @@
 // Description:
 //   ユーザ語録
 // Commands:
-//   goroku2 <name> add <message> - 語録を追加
-//   goroku2 <name> del <message> - 語録を削除
-//   goroku2 <name> random <message> - 語録をランダム表示
-//   goroku2 <name> list <message> - 語録一覧
+//   goroku2 add <name> <message> - 語録を追加
+//   goroku2 rm <name> <message> - 語録を削除
+//   goroku2 random <name> <message> - 語録をランダム表示
+//   goroku2 list <name> <message> - 語録一覧
 
 'use strict';
 
@@ -13,7 +13,7 @@ module.exports = robot => {
 
 	const addName = name => {
 		const logger = robot.brain.get('gorokuNames') || [];
-		if (logger.contains(name)) {
+		if (logger.indexOf(name) >= 0) {
 			return;
 		}
 		logger.push(name);
@@ -22,34 +22,40 @@ module.exports = robot => {
 
 	const loadLogger = name => {
 		addName(name);
-		return robot.brain.get(`goroku_${name}`) || [];
+		return robot.brain.get(`goroku/${name}`) || [];
 	};
 
-	robot.hear(/^goroku2 add (.+) (.+)/i, msg => {
-		const [name, m] = msg.match;
+	robot.hear(/^goroku2 add (.+) (.+)$/i, msg => {
+		const [_, name, m] = msg.match;
 		const logger = loadLogger(name);
 		logger.push(m);
-		robot.brain.set(`goroku_${name}`, logger);
+		robot.brain.set(`goroku/${name}`, logger);
 		msg.send(`${name} ${m}`);
 	});
 
-	robot.hear(/^goroku2 del (.+) (.+)$/, msg => {
-		const [name, m] = msg.match;
+	robot.hear(/^goroku2 rm (.+) (.+)$/, msg => {
+		const [_, name, m] = msg.match;
 		const logger = loadLogger(name);
 		const nextLogger = logger.filter(mes => mes !== m);
-		robot.brain.set(`goroku_${name}`, nextLogger);
+		robot.brain.set(`goroku/${name}`, nextLogger);
 	});
 
 	robot.hear(/^goroku2 random (.+)$/i, msg => {
-		const name = msg.match;
+		const name = msg.match[1];
 		const logger = loadLogger(name);
 		const mes = logger[Math.floor(Math.random() * logger.length)];
 		msg.send(`${name} ${mes}`);
 	});
 
 	robot.hear(/^goroku2 list (.+)$/i, msg => {
-		const name = msg.match;
+		const name = msg.match[1];
 		const logger = loadLogger(name);
 		msg.send(`${logger.map(v => `${name} ${v}`).join('\n')}`);
+	});
+
+	robot.hear(/^goroku2 list-all$/i, msg => {
+		const names = robot.brain.get('gorokuNames') || [];
+		const text = names.map(name => loadLogger(name).map(v => `${name} ${v}`).join('\n')).join('\n');
+		msg.send(text);
 	});
 };
