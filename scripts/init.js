b930d3b92de47dc0a05ae9ad3fc149dd01166485
akameco <akameco.t@gmail.com>
2016-10-26 18:35:43 +0900

    Add init.js

---
+++ scripts/init.js
@@ -0,0 +1,13 @@
+// Description:
+//	herokuへpingを送り続けるスクリプト
+
+'use strict';
+const herokuUrl = 'https://cpsbot-bot.herokuapp.com/';
+
+require('heroku-self-ping')(herokuUrl);
+
+module.exports = robot => {
+	const chanel = 'test';
+	const msg = 'そして次の残留が始まるのです';
+	robot.send(chanel, msg);
+};
--- scripts/self-ping.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-require('heroku-self-ping')('https://cpsbot-bot.herokuapp.com/');
-
-module.exports = () => {
-	console.log('load heroku-self-ping');
-};
