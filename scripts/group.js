a355e4ce0092ba46fc1d7f65090a6285e0897090
elzup <guild0105@gmail.com>
2017-05-19 13:23:38 +0900

    Update grade groups

--- scripts/group.js
+++ scripts/group.js
@@ -8,19 +8,19 @@
 module.exports = robot => {
 	robot.brain.autoSave = true;
 
-	robot.hear(/@b3/i, msg => {
-		msg.send(`@atsuo @fjsmu @hashikazu @hayashi @jazzmas0531 @kodama_sakana @makky @naoki @sarise @seiyanakamura @wada`);
-	});
-
 	robot.hear(/@b4/i, msg => {
-		msg.send(`@adachi @akameco @hiwatashi @iwamoto @kudo @morosawa @muni @shimada @tsukada @yuu`);
+		msg.send(`@atsuo @fjsmu @hashikazu @hayashi @jazzmas0531 @kodama_sakana @makky @sarise @seiyanakamura @wada`);
 	});
 
 	robot.hear(/@m1/i, msg => {
-		msg.send(`@elzup @naoki @naoya @ninten @sukonbu`);
+		msg.send(`@adachi @akameco @kudo`);
 	});
 
 	robot.hear(/@m2/i, msg => {
+		msg.send(`@elzup @naoki @naoya @ninten @sukonbu`);
+	});
+
+	robot.hear(/@m3/i, msg => {
 		msg.send(`@kondo @kyosuke @misaki @mitsuha_suxee @shinji_cps @shino @tk @tsuno @yone`);
 	});
 };
