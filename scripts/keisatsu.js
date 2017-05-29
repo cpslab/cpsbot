888e39aa3d2bcc5d53389e03b532877a1a29a313
elzup <guild0105@gmail.com>
2017-05-29 12:00:26 +0900

    Kotlin 警察

--- scripts/keisatsu.js
+++ scripts/keisatsu.js
@@ -9,4 +9,8 @@ module.exports = robot => {
 	robot.hear(/[Gg]ithub/, msg => {
 		msg.send(`ピピピー！GitHub 警察だ！`);
 	});
+
+	robot.hear(/コリン|kotrin/i, msg => {
+		msg.send(`ピピピー！Kotlin 警察だ！`);
+	});
 };
