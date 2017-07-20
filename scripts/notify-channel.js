d286b28ba00a1d79aa3e19e01293d9b3551c4925
elzup <guild0105@gmail.com>
2017-07-20 13:13:03 +0900

    🤔

--- scripts/notify-channel.js
+++ scripts/notify-channel.js
@@ -2,9 +2,11 @@
 //    notify create channel on Slack in #new_channels
 //
 module.exports = robot => {
-	const slack = robot.adapter.client;
-
-	slack.on('raw_messages', msg => {
+	console.log(robot.adapter.client);
+	if (!(robot.adapter.client && robot.adapter.client.rtm && robot.adapter.client.rtm.on)) {
+		return;
+	}
+	robot.adapter.client.rtm.on('raw_messages', msg => {
 		if (msg && msg.type === 'channel_created') {
 			robot.send({root: 'random'}, `新しいチャンネル #${msg.channel.id}が作られたよ！`);
 		}
