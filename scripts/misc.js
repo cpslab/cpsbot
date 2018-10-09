714bc08466367883c3116cfdeecaa9f9987f960a
hamajun <rodhamjun@hotmail.com>
2018-10-09 12:18:05 +0900

    Add linus response
    遅れます|遅れそう|遅刻します
    => :linus:

--- scripts/misc.js
+++ scripts/misc.js
@@ -8,4 +8,8 @@ module.exports = robot => {
 	robot.hear(/^最高かよ$/i, msg => {
 		msg.send('https://files.slack.com/files-pri/T02TM1NQZ-F2SB6JLN5/2958.jpg');
 	});
+	
+	robot.hear(/遅れます|遅れそう|遅刻します/i, msg => {
+		msg.send(':linus:');
+	});
 };
