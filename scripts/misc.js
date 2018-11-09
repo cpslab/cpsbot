294ba05f5d375d81a0e6658bf66cdf357f6f4820
hamajun <rodhamjun@hotmail.com>
2018-11-09 20:30:20 +0900

    Goodbye Linus...

--- scripts/misc.js
+++ scripts/misc.js
@@ -9,7 +9,7 @@ module.exports = robot => {
 		msg.send('https://files.slack.com/files-pri/T02TM1NQZ-F2SB6JLN5/2958.jpg');
 	});
 
-	robot.hear(/遅れます|遅れそう|遅刻します/i, msg => {
-		msg.send(':linus:');
-	});
+// 	robot.hear(/遅れます|遅れそう|遅刻します/i, msg => {
+// 		msg.send(':linus:');
+// 	});
 };
