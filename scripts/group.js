2aa0a2bcd739d8fa41db1a9518a34864fb3ce8f7
RoneLoreto <ryo1ito.hkdspr@gmail.com>
2020-03-29 16:09:29 +0900

    update name list
    (b3 -> b4, -> m1, m1 -> m2, -> ob)

--- scripts/group.js
+++ scripts/group.js
@@ -1,15 +1,16 @@
 // Description:
 //   メンバーグループ呼び出し
 // Commands:
-//   @b4, @m1, @m2, @m3, @m4, @現役
+//   @b4, @m1, @m2, @現役
 //
 // UserID は以下のURLからCSVをダウンロードすることで確認できる https://iwailab.slack.com/admin
 // または以下のAPIをたたく https://api.slack.com/methods/users.list/test
 // また、基本的に発表スケジュールの欄にSlack ID をいれて管理している。 2019発表スケジュール: https://docs.google.com/spreadsheets/d/1TzGpyJmyKFodQu4hc1-JLsa9CDlapN72GEDs8S1fpJo/edit#gid=0
 
 'use strict';
+const b3 = [];
 
-const b3 = [
+const b4 = [
   'UP3D0AU8H',
   'UPBMGFG0P',
   'UNX2T1DCJ',
@@ -23,27 +24,9 @@ const b3 = [
   'UDC4MJV89',
 ];
 
-const b4 = [
-  'UA0BM185P',
-  'UBDRCAW5A',
-  'UDA9CPXL0',
-  'UDB2E282U',
-  'UDB7753B7',
-  'UDC01L52P',
-  'UDC3YKWTB',
-  'UDC4MJV89',
-  'UDCBK7N1L',
-  'UDCGHQ283',
-  'UDCJG2Y9H',
-  'UDCR4C8LF',
-  'UDD8RU8UW',
-  'UDDP55JMD',
-  'UDVJUV4UE',
-];
+const m1 = ['UA0BM185P', 'UDCJG2Y9H', 'UDC01L52P', 'UDA9CPXL0', 'UDCR4C8LF'];
 
-const m1 = ['U7HMT51QS', 'U7H2PMXUG'];
-
-const m2 = ['U2LGU5T8U', 'U2LH9MMCH', 'U2LH8H9JB', 'U2LH9NX1D', 'U2LGYNFEC'];
+const m2 = ['U7HMT51QS', 'U7H2PMXUG'];
 
 const ob = [
   'U0C9WJNBB',
@@ -76,6 +59,22 @@ const ob = [
   'U7JJFGB9U',
   'U7H6LTT7T',
   'U7LS7T6GN',
+  'U2LGU5T8U',
+  'U2LH9MMCH',
+  'U2LH8H9JB',
+  'U2LH9NX1D',
+  'U2LGYNFEC',
+  'UA0BM185P',
+  'UBDRCAW5A',
+  'UDB2E282U',
+  'UDB7753B7',
+  'UDC3YKWTB',
+  'UDC4MJV89',
+  'UDCBK7N1L',
+  'UDCGHQ283',
+  'UDD8RU8UW',
+  'UDDP55JMD',
+  'UDVJUV4UE',
 ];
 
 // 現役
@@ -90,9 +89,11 @@ const toMentionFormat = array =>
 module.exports = robot => {
   robot.brain.autoSave = true;
 
+  /* B3が配属されたらコメントアウトを外す
   robot.hear(/@b3/i, msg => {
     msg.send(toMentionFormat(b3));
   });
+  */
 
   robot.hear(/@b4/i, msg => {
     msg.send(toMentionFormat(b4));
